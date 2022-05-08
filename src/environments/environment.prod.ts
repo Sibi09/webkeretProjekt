8f98a7a47fa8e71efb06942244dc706dea725fd5
Sibi666 <59481697+Sibi666@users.noreply.github.com>
2022-05-08 12:17:55 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   firebase: {
-    projectId: 'projektem-9a12f',
-    appId: '1:45322075223:web:3f7dcaeb8225fbaf7bb712',
-    storageBucket: 'projektem-9a12f.appspot.com',
-    apiKey: 'AIzaSyAtivkRui9WNgXrpA4Qv23P-drFKeWSk7s',
-    authDomain: 'projektem-9a12f.firebaseapp.com',
-    messagingSenderId: '45322075223',
-    measurementId: 'G-D1SJ887G54',
+
   },
   production: true
 };
